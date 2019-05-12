85c0f0602f0b59eb6148275c732fe3934f000bb7
Karim <karim.rochat@heig-vd.ch>
2019-05-12 15:45:22 +0200

    Avancement

--- src/index.js
+++ src/index.js
@@ -8,21 +8,29 @@
 //https://github.com/idris-maps/exemple-transformation-de-donnees-avec-node/blob/master/drawGraph.js
 //https://github.com/idris-maps/heig-datavis-2019/blob/master/20190301-manipulation-dom/exemples/exemple_1.html
 
+
+
 /*
 * Importation des librairies et des fichiers
 */
 
 const fetch = require('node-fetch')
 const d3 = require('d3')
-const billboard = require('billboard')
 const moment = require('moment')
-const http = require('http')
 const data = require('../data/donnees.json')
 
 
 
 
+/*
+* Définition des éléments HTML
+*/
 
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight
+const container = DOM.svg(WIDTH, HEIGHT)
+const svg = d3.select(container)
+const body = d3.select('body')
 
 
 
@@ -35,70 +43,17 @@ const data = require('../data/donnees.json')
 * Représentation des points
 */
 
-const WIDTH = window.innerWidth
-const HEIGHT = window.innerHeight
-const container = DOM.svg(WIDTH, HEIGHT)
-const svg = d3.select(container)
-const points = svg.append('g')
-const body = d3.select('body')
 
-/*
-//Pour chaque objet dans mon fichier des données que j'ajoute, un nouveau point est créé avec ses attributs
-points.selectAll('point')
+ const points = svg.append('g')
+  
+  points.selectAll('circle')
     .data(data)
     .enter()
-    .append('point')
-    .attr('x', xScale)
-    .attr('y', yScale)
-    .attr('r', diametreScale)
-    .attr('fill', colorScale)
-*/
-
-
-/* Des morceaux de codes issus de l'exemple
-
-export const svg = body.append('svg')
-  .attr('viewBox', `0 0 ${WIDTH} ${HEIGHT}`)
-
-export const xAxis = d3.axisBottom()
-  .ticks(4)
-export const bottomAxis = svg.append('g')
-  .attr('transform', `translate(0, ${HEIGHT - MARGIN})`)
-export const yAxis = d3.axisLeft()
-export const leftAxis = svg.append('g')
-  .attr('transform', `translate(${MARGIN}, 0)`)
-
-const body = d3.select('body')
-export const selectX = body.append('select')
-export const selectY = body.append('select')
-
-Peux-être faire un addEventListener sur chaque point pour gérer le click
-
     .append('circle')
-      .attr('cx', CIRCLE_RADIUS)
-      .attr('cy', CIRCLE_RADIUS)
-      .attr('r', CIRCLE_RADIUS)
-
-*/
-
-/* Bout de code pouvant être intéressant
-const graph = DATA =>
-`<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
-${data.map((pays,i) => `<rect width="90" height="${pays.somme*8}" x="${(i)*100}" y="0" fill="blue" />
-<text x="${(i)*100}" y="${(100)}" font-family="Comic Sans MS" font-size="12" fill="black" >${pays.p} ${"="} ${pays.somme} </text>`)}
-</svg>`
-*/
-
-/*
-const graphique = data =>
-'<svg width=WIDTH height=HEIGHT xmlns="http://www.w3.org/2000/svg">
-${data.map(Object, i) => '<circle r=diametreScale fill=colorScale x=xScale y=yScale/>'}
-</svg>'
-*/
-
-
-
-
+    .attr('x', d => xScale(d.Date))
+    .attr('y', d => yScale(d.Vinfinity))
+    .attr('fill', d => colorScale(d.DMinimum))
+    .attr('r', d => diametreScale(d.Diameter))
 
 
 
@@ -114,8 +69,8 @@ ${data.map(Object, i) => '<circle r=diametreScale fill=colorScale x=xScale y=ySc
 */
 
 const diametreScale = d3.scaleLinear
-    .domain((d3.min(data, d => d.Diameter), d3.max(data, d => d.Diameter)))
-    .range([0, 10])
+.domain((d3.min(data, d => d.Diameter), d3.max(data, d => d.Diameter)))
+.range([0, 10])
 
 
 
@@ -124,49 +79,33 @@ const diametreScale = d3.scaleLinear
 */
 
 const colorScale = d3.scaleLinear
-    .domain([d3.min(data, d => d.DMinimum, d3.max(data, d=>d.DMinimum))])
-    .range([blue, red])
+.domain([d3.min(data, d => d.DMinimum, d3.max(data, d=>d.DMinimum))])
+.range([blue, red])
+
+
 
 /*
 * Axe vertical
 */
 
 const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d=>d.Vinfinity)])
-    .range([0, 35000])
+.domain([0, d3.max(data, d=>d.Vinfinity)])
+.range([35000, 0])
 
 const axisY = d3.axisLeft()
 .scale(yScale)
 .ticks(7)
-.tickFormat(d)
+
 
 
 /*
 * Axe horizontal
 */
 
-//Utiliser moment pour obtenir les dates en ms
-
 const xScale = d3.scaleLinear()
-    .domain([d3.min(data, d=>d.Date), d3.max(data, d=>d.Date)])
-    .range([-30, 30])
+.domain([d3.min(data, d=>d.Date), d3.max(data, d=>d.Date)])
+.range([-30, 30])
 
 const axisX = d3.axisBottom()
 .scale(xScale)
 .ticks(12)
-.tickFormat(x)
-
-
-
-
-/*
-* Nom de l'astéroïde
-*/
-
-//Peut-être avec un hover
-
-
-/*
-* Checkboxes (optionnel, on verra comment ça se passe)
-*/
-
