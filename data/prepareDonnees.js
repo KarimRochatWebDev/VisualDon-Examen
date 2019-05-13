cdf733d476691ea4da874aabd0bb1100e90798d4
Karim <karim.rochat@heig-vd.ch>
2019-05-13 13:16:43 +0200

    Examen terminé !

--- data/prepareDonnees.js
+++ data/prepareDonnees.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const d3 = require('d3')
 const moment = require('moment')
 
-const csv = fs.readFileSync('../data/datav2.csv', 'utf-8')
+const csv = fs.readFileSync('../data/data.csv', 'utf-8')
 const data = d3.csvParse(csv);
 
-fs.writeFileSync('datav2.json', JSON.stringify(data), 'utf-8')
+fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8')
